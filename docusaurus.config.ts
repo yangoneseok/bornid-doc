0765c9d78205e08abc1364a8257806288dd04221
Wonseok Yang <wsyang@digicaps.com>
2025-06-12 15:25:15 +0900

    Fix broken links: update footer link from /docs/intro to /docs/

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,7 +91,7 @@ const config: Config = {
           items: [
             {
               label: '시작하기',
-              to: '/docs/intro',
+              to: '/docs/',
             },
           ],
         },
