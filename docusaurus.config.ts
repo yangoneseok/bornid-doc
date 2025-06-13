7cf440a6e7e741ffa5a0f4fe9fb27ebd04053d5b
Wonseok Yang <wsyang@digicaps.com>
2025-06-13 14:17:56 +0900

    feat: 커스텀 도메인 설정 (doc.bornid.io)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://yangoneseok.github.io',
+  url: 'https://doc.bornid.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/bornid-doc/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
