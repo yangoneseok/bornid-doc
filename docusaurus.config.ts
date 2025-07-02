931f56877644966754b7fcfc20dd451ca754c64f
Wonseok Yang <wsyang@digicaps.com>
2025-07-02 10:24:12 +0900

    doc -> docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://doc.bornid.io',
+  url: 'https://docs.bornid.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
